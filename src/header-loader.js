66789fe4a74b256c62c4617cab1af04ba5e1c4b0
Kevin Ah-son <ahsonkevin@gmail.com>
2024-10-28 21:59:27 +0100

    edit path

--- src/header-loader.js
+++ src/header-loader.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const headerHTML = `
         <head>
-            <link rel="manifest" href="/manifest.json">
+            <link rel="manifest" href="../manifest.json">
             <meta name="theme-color" content="#2196F3">
             <script>
                 if ('serviceWorker' in navigator) {
-                    navigator.serviceWorker.register('/service-worker.js')
+                    navigator.serviceWorker.register('../service-worker.js')
                         .then(registration => {
                             console.log('ServiceWorker registration successful');
                         })
@@ -17,13 +17,13 @@ document.addEventListener('DOMContentLoaded', function() {
             <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="/style.css">
+            <link rel="stylesheet" href="../style.css">
             <title></title>
         </head>
         <nav>
             <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="quiz.html">Quiz</a></li>
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="../quiz.html">Quiz</a></li>
             </ul>
         </nav>`;
 
