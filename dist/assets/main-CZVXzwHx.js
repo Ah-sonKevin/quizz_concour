d996d307b7e888dbf48d573e18e688095142586c
khyxes <ahsonkevin@gmail.com>
2024-10-29 08:10:41 +0100

    Delete dist directory

--- dist/assets/main-CZVXzwHx.js
+++
@@ -1,1 +0,0 @@
-import{l as e}from"./functions-E1wsBcZX.js";window.addEventListener("DOMContentLoaded",e);
--- dist/assets/quiz-EPj8hjtR.js
+++
@@ -1,1 +0,0 @@
-import{b as i,s as o}from"./functions-E1wsBcZX.js";window.loadQuiz=i;window.submitQuiz=o;
